fa25940781ce0b1b6f7d073f084e734c48f5da76
Spektr <spektr1987@rambler.ru>
2016-06-20 22:40:29 +0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {Routes, ROUTER_DIRECTIVES} from '@angular/router';
-
+import {BookService} from './services/book.service';
+import {DataService} from "./services/data.service";
 import {HomeComponent} from './components/home/home.component'
 
 @Component({
-  selector: 'app-component',
-  providers: [],
-  pipes: [],
-  directives: [ROUTER_DIRECTIVES],
-  template: require('app/app.component.html')
+    selector: 'app-component',
+    encapsulation: ViewEncapsulation.None,
+    providers: [DataService, BookService],
+    pipes: [],
+    directives: [ROUTER_DIRECTIVES],
+    styles:[`
+        html,body{
+            height: 100%;}
+                       
+        .app-component{
+            min-height: 100%;
+            min-width: 300px;
+            background-color: #f5f5f5;}
+            
+            .app-component__header{
+                margin-top: 10px;}
+                
+            .app-component__content{}
+            
+            .app-component__footer{
+                margin: 10px 0;}
+            
+            .app-component__brand{}
+                .app-component__brand img{
+                    float:left;
+                    padding-right: 5px;
+                    max-height: 100%;}
+            
+            .app-component .container{
+                background-color: white;}
+    `],
+    template: require('app/app.component.html')
 })
 @Routes([
   { path: '/',       component: HomeComponent }
 ])
-export class AppComponent {
-
-  constructor() {}
-
-}
+export class AppComponent {}
