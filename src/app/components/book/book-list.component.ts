5be6f49f3b8fa6d8d17519c4e0f80ee7d7929c07
Spektr <spektr1987@rambler.ru>
2016-06-24 23:14:24 +0300

    add sorting and images to list

--- src/app/components/book/book-list.component.ts
+++ src/app/components/book/book-list.component.ts
@@ -1,33 +1,49 @@
 import { Component, EventEmitter, OnInit } from '@angular/core';
 import {IBook} from '../../interfaces/IBook';
-import {BookService} from '../../services/book.service';
+import {DataService} from '../../services/data.service'
 
 @Component({
     moduleId: module.id,
     selector: 'book-list-component',
-    outputs:['bookChanges'],
+    inputs:['aBook'],
+    outputs:['selectBookChange'],
     styles:[`
         .book-list-component{}
         
             .book-list-component .center-block{
-                margin-bottom: 3px;}        
+                margin-bottom: 3px;}   
+                
+            .book-list-component td{
+                vertical-align: middle;}    
+                 
+            .book-list-component th{
+                text-align:center;
+                vertical-align: middle;}        
     `],
     template: require('./book-list.component.html')
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit{
 
-    bookChanges:EventEmitter<IBook> = new EventEmitter();
+    selectBookChange:EventEmitter<IBook> = new EventEmitter();
     aBook:IBook[]=null;
 
     constructor(
-        private _bookService:BookService
-    ) {}
+        private _dataService:DataService
+    ){}
 
-    ngOnInit() {
-        this.aBook = this._bookService.list();
+    ngOnInit(){
+        let sorting:string = this._dataService.getSorting();
     }
 
     selectBook(book:IBook){
-        this.bookChanges.emit(book);
+        this.selectBookChange.emit(book);
+    }
+
+    sortBy(name:string, asc?:boolean){
+        let self = this,
+            negative = asc?1:-1;
+
+        self.aBook.sort((first:IBook,second:IBook)=>{return negative*(first[name].trim()<second[name].trim()?1:-1);});
+        self._dataService.setSorting(name);
     }
 }
